a138bea500d89693d5978fb05585986c9cfc0c0b
abhidharshss <abhidharsh1999@gmail.com>
2023-06-11 18:56:27 +0530

    deletion of phone number field

--- authentication/models.py
+++ authentication/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth .models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
-from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
